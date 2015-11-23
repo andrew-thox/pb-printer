2056b8a1ce06df06808240ac22cdd241fd546c56
Andrew Kenyon <andrewkenyon123@gmail.com>
2015-11-23 21:56:13 +0000

    Initial Commit

---
+++ src/printer/core.clj
@@ -0,0 +1,7 @@
+(ns printer.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/printer/core_test.clj
@@ -0,0 +1,7 @@
+(ns printer.core-test
+  (:require [clojure.test :refer :all]
+            [printer.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
