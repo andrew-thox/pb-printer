7b55b573f530376cb113a8381a9b59ad220fd7ea
Andrew Kenyon <andrewkenyon123@gmail.com>
2016-01-07 23:38:08 +0000

    avro schemas for nosql graph schema, these are currently strictly experimental

--- src/printer/schemas.clj
+++ src/printer/schemas.clj
@@ -15,3 +15,47 @@
              {:name "publication", :type "string"}
              {:name "publish_date", :type "long"}
              {:name "acquistion_date", :type "long"}]}))
+
+(def article
+  (avro/parse-schema
+    {:type :record
+     :name "Article"
+     :fields [{:name "id", :type "string"}]}))
+
+(def article-property-value
+  (avro/parse-schema
+    {:type :record
+     :name "ArticlePropertyValue"
+     :fields [{:name "title" :type ["string", :null]}
+              {:name "publication_date" :type ["long", :null]}
+              {:name "external_unique_identifier" :type ["string", :null]}
+              {:name "internal_unique_identifier" :type ["string", :null]}
+              {:name "url" :type ["string", :null]}
+              {:name "text" :type ["bytes", :null]}]}))
+
+(def article-property
+  (avro/parse-schema
+    {:type :record
+     :name "ArticleProperty"
+     :fields [{:name "article" :type "Article"}
+              {:name "article_property_value" :type "ArticlePropertyValue"}]}))
+
+(def data-unit
+  (avro/parse-schema
+    {:type :record
+     :name "DataUnit"
+     :fields [{:name "article_property" :type "ArticleProperty"}]}))
+
+;TODO: Add source to pedigree
+(def pedigree
+  (avro/parse-schema
+    {:type :record
+     :name "Pedigree"
+     :field [{:name "timestamp" :type "long"}]}))
+
+(def data
+  (avro/parse-schema
+    {:type :record
+     :name "Data"
+     :fields [{:name "data_unit" :type "DataUnit"}
+              {:name "pedigree" :type "Pedigree"}]}))
