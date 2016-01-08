36617c424d7c7c29c55f0f633393407ce0498a41
Andrew Kenyon <andrewkenyon123@gmail.com>
2016-01-08 23:47:34 +0000

    split out schema maps and schemas, if you wish to include a type which is defined in another schema, that schema map is a dependency and must be included as an arg to avro/parse-schema

--- src/printer/schemas.clj
+++ src/printer/schemas.clj
@@ -16,46 +16,75 @@
              {:name "publish_date", :type "long"}
              {:name "acquistion_date", :type "long"}]}))
 
-(def article
-  (avro/parse-schema
+
+;---These are outbound schemas---
+;TODO: Indent properly
+;Schema maps
+
+(def article-map
     {:type :record
      :name "Article"
-     :fields [{:name "id", :type "string"}]}))
+     :fields [{:name "id", :type "string"}]})
 
-(def article-property-value
-  (avro/parse-schema
-    {:type :record
+(def article-propery-value-map
+  {:type :record
      :name "ArticlePropertyValue"
      :fields [{:name "title" :type ["string", :null]}
               {:name "publication_date" :type ["long", :null]}
               {:name "external_unique_identifier" :type ["string", :null]}
               {:name "internal_unique_identifier" :type ["string", :null]}
               {:name "url" :type ["string", :null]}
-              {:name "text" :type ["bytes", :null]}]}))
+              {:name "text" :type ["bytes", :null]}]})
 
-(def article-property
-  (avro/parse-schema
+(def article-property-map
     {:type :record
      :name "ArticleProperty"
      :fields [{:name "article" :type "Article"}
-              {:name "article_property_value" :type "ArticlePropertyValue"}]}))
+              {:name "article_property_value" :type "ArticlePropertyValue"}]})
 
-(def data-unit
-  (avro/parse-schema
-    {:type :record
+(def data-unit-map
+  {:type :record
      :name "DataUnit"
-     :fields [{:name "article_property" :type "ArticleProperty"}]}))
+     :fields [{:name "article_property" :type "ArticleProperty"}]})
 
 ;TODO: Add source to pedigree
-(def pedigree
-  (avro/parse-schema
-    {:type :record
+(def pedigree-map
+  {:type :record
      :name "Pedigree"
-     :field [{:name "timestamp" :type "long"}]}))
+     :fields [{:name "timestamp" :type "long"}]})
 
-(def data
-  (avro/parse-schema
+(def data-map
     {:type :record
      :name "Data"
      :fields [{:name "data_unit" :type "DataUnit"}
-              {:name "pedigree" :type "Pedigree"}]}))
+              {:name "pedigree" :type "Pedigree"}]})
+
+;Actual Avro schemas
+
+(def article
+  (avro/parse-schema article-map))
+
+(def article-property-value
+  (avro/parse-schema article-propery-value-map))
+
+(def article-property
+  (avro/parse-schema article-map
+                     article-propery-value-map
+                     article-property-map))
+
+(def data-unit
+  (avro/parse-schema article-map
+                     article-propery-value-map
+                     article-property-map
+                     data-unit-map))
+
+(def pedigree
+  (avro/parse-schema pedigree-map))
+
+(def data
+  (avro/parse-schema article-map
+                     article-propery-value-map
+                     article-property-map
+                     data-unit-map
+                     pedigree-map
+                     data-map))
