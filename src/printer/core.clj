66418bce0cbb40226232da1dbcc1cc469a95407a
Andrew Kenyon <andrewkenyon123@gmail.com>
2017-05-02 23:01:32 +0100

    the article consumer will now put an article in elasticsearch

--- src/printer/core.clj
+++ src/printer/core.clj
@@ -9,11 +9,6 @@
             [clj-statsd :as statsd]))
 
 (defn -main [& args]
-  ;TODO: This should log/alert
-  (try
-    (statsd/setup (env :statd-host) (env :statd-port))
-  (catch Exception e (str "caught exception: " (.getMessage e))))
-
   (let [conn  (rmq/connect {:uri (env :amqp-url)})
         ch    (lch/open conn)]
     (println (format "[main] Connected. Channel id: %d" (.getChannelNumber ch)))
--- src/printer/database_adapter.clj
+++ src/printer/database_adapter.clj
@@ -3,13 +3,20 @@
             [jdbc.pool.c3p0 :as pool]
             [environ.core :refer [env]]
             [pandect.algo.sha256 :refer :all]
-            [clj-statsd :as statsd])
+            [qbits.spandex :as s]
+            [qbits.spandex.utils :as s-utils])
   (:import (java.net URI)))
+
+(def c (s/client {:hosts [(env :aws-elastic-url)]}))
 
 (defn consume-article [article]
   (let [hash (str (sha256 (clojure.string/join " " [(:author article) (:title article) (:link article)])))]
     (if false nil
       (do
         (println (conj article {:hash hash}))
-        (statsd/increment (clojure.string/lower-case (str (:publication article) "_articles_processed")))
-        (statsd/increment "articles_processed")))))
+        (s/request c {:url (s-utils/url ["customer" "external"])
+              :method :post
+              :body article})
+        (println "NEW ARTICLE")
+        (System/exit 0)
+        ))))
